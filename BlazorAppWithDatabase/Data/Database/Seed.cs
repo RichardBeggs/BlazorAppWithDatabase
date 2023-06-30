84edee7ad6932fb20bed9a5a22ec6541722e356f
Richard Beggs <richardbe@kainos.com>
2023-06-30 13:32:31 +0100

    Update seeding

--- BlazorAppWithDatabase/Data/Database/Seed.cs
+++ BlazorAppWithDatabase/Data/Database/Seed.cs
@@ -14,6 +14,10 @@ namespace BlazorAppWithDatabase.Data.Database
             {
                 context.Pubs.Add(new Pub { Name = "The Parlour", AddressLine1 = "2-4 Elmwood Ave.", AddressLine2 = "", City = "Belfast" });
                 context.Pubs.Add(new Pub { Name = "House Belfast", AddressLine1 = "59-63 Bontanic Ave.", AddressLine2 = "", City = "Belfast" });
+                context.Pubs.Add(new Pub { Name = "The Stables at Hilden Brewery", AddressLine1 = "192 Grand St", AddressLine2 = "", City = "Lisburn" });
+                context.Pubs.Add(new Pub { Name = "The Three Crowns", AddressLine1 = "21 Bridge St", AddressLine2 = "", City = "Lisburn" });
+                context.Pubs.Add(new Pub { Name = "The Bryansburn Inn", AddressLine1 = "151 Bryansburn Road.", AddressLine2 = "", City = "Bangor" });
+                context.Pubs.Add(new Pub { Name = "Donegans Restaurant", AddressLine1 = "37-39 High Street.", AddressLine2 = "", City = "Bangor" });
                 context.SaveChanges();
             }
 
